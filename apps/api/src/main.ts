da9d66887969bb8af12cce937fecc640a49fef21
Aleksandr Lihih <sanshan@users.noreply.github.com>
2026-08-18 18:15:51 +0200

    fix: use API_PORT for Nest runtime

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   app.enableShutdownHooks();
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  const port = Number(process.env.API_PORT ?? 3000);
   await app.listen(port);
   Logger.log(`Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
