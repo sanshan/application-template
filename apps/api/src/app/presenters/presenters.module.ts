c865268ead82cbd4e5b98455dbbe01b7d4d9e747
Aleksandr Lihih <sanshan@users.noreply.github.com>
2026-08-18 13:19:08 +0200

    feat(api): wire health presentation

--- apps/api/src/app/presenters/presenters.module.ts
+++ apps/api/src/app/presenters/presenters.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TerminusModule } from '@nestjs/terminus';
+
 import { ApplicationModule } from '../application/application.module';
+import { DatabaseHealthIndicator } from './http/health/database-health.indicator';
+import { HealthController } from './http/health/health.controller';
 
 @Module({
-    imports: [ApplicationModule],
+    imports: [ApplicationModule, TerminusModule],
+    controllers: [HealthController],
+    providers: [DatabaseHealthIndicator],
 })
 export class PresentersModule {}
