5612a3b5ef7777da3b9046c7ad3694a7b9b2c528
Aleksandr Lihih <sanshan@users.noreply.github.com>
2026-08-18 13:24:48 +0200

    refactor(api): update presenter indicator import

--- apps/api/src/app/presenters/presenters.module.ts
+++ apps/api/src/app/presenters/presenters.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
 
 import { ApplicationModule } from '../application/application.module';
-import { DatabaseHealthIndicator } from './http/health/database-health.indicator';
+import { DatabaseHealthIndicator } from './http/health/indicators/database-health.indicator';
 import { HealthController } from './http/health/health.controller';
 
 @Module({
