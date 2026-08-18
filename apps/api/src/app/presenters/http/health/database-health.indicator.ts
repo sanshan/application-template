a978b81d11ea72924a91ac613652cd2f366cf956
Aleksandr Lihih <sanshan@users.noreply.github.com>
2026-08-18 13:18:48 +0200

    feat(api): add database health indicator

---
+++ apps/api/src/app/presenters/http/health/database-health.indicator.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { HealthIndicatorService } from '@nestjs/terminus';
+
+import { CheckDatabaseHealthUseCase } from '../../../application/use-cases/system/health-check/check-database-health/check-database-health.use-case';
+
+@Injectable()
+export class DatabaseHealthIndicator {
+    constructor(
+        private readonly checkDatabaseHealthUseCase: CheckDatabaseHealthUseCase,
+        private readonly healthIndicatorService: HealthIndicatorService,
+    ) {}
+
+    async check() {
+        const indicator = this.healthIndicatorService.check('database');
+
+        try {
+            await this.checkDatabaseHealthUseCase.execute();
+
+            return indicator.up();
+        } catch {
+            return indicator.down();
+        }
+    }
+}
