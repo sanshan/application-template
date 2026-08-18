64d9f1caa8f6a45790df164318b931f25746b150
Aleksandr Lihih <sanshan@users.noreply.github.com>
2026-08-18 12:57:25 +0200

    feat(api): register database health use case

--- apps/api/src/app/application/application.module.ts
+++ apps/api/src/app/application/application.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+
 import { InfrastructureModule } from '../infrastructure/infrastructure.module';
+import { CheckDatabaseHealthUseCase } from './use-cases/system/health-check/check-database-health/check-database-health.use-case';
 
 @Module({
     imports: [InfrastructureModule],
+    providers: [CheckDatabaseHealthUseCase],
+    exports: [CheckDatabaseHealthUseCase],
 })
 export class ApplicationModule {}
